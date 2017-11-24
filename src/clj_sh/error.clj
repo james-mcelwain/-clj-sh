ac5cd3a89c92275ce9df52fd3734e5571346a873
James McElwain <jmcelwain@gmail.com>
2017-11-24 11:40:16 -0600

    make callback to unwrap optional

--- src/clj_sh/error.clj
+++ src/clj_sh/error.clj
@@ -1,12 +1,13 @@
 (ns clj-sh.error
   (:require [clojure.core.match :refer [match]]))
 
-(defn unwrap [either f]
-  (match either
-         [:left err] err
-         [:right val] (f val)))
+(defn unwrap
+  ([either] (unwrap either identity))
+  ([either f] (match either
+                     [:left err] err
+                     [:right val] (f val))))
 
-(defn EEXIST [file] (str "File xists ") file)
+(defn EEXIST [file] (str "File exists ") file)
 
 (defn ENOENT [file] (str "No such file or directory " file))
 
